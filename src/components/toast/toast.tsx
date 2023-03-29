240531bc1adcd807efe773010333a1f67836e5b1
Yuri Skripal <yjura@protonmail.com>
2023-03-29 17:05:17 +0300

    refactor: change header title logic

--- src/components/toast/toast.tsx
+++ src/components/toast/toast.tsx
@@ -1,24 +1,18 @@
-import { Component } from 'react';
-
 interface Props {
   showToast: boolean;
 }
 
-export class Toast extends Component<Props> {
-  render() {
-    return (
-      <div
-        className={`toast fade text-white text-center bg-info ${
-          this.props.showToast ? 'show' : ''
-        }`}
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
-      >
-        <div className="toast-body" data-testid="toast-id">
-          Card succesfully added.
-        </div>
+export const Toast = ({ showToast }: Props) => {
+  return (
+    <div
+      className={`toast fade text-white text-center bg-info ${showToast ? 'show' : ''}`}
+      role="alert"
+      aria-live="assertive"
+      aria-atomic="true"
+    >
+      <div className="toast-body" data-testid="toast-id">
+        Card succesfully added.
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
