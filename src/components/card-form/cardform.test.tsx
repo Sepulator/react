e6094037a1cf3755f803f014858c64da252776c4
Yuri Skripal <yjura@protonmail.com>
2023-04-13 16:26:15 +0300

    refactor: don't close modal on card click

--- src/components/card-form/cardform.test.tsx
+++ src/components/card-form/cardform.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, vi } from 'vitest';
+import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import { CardForm } from './cardform';
