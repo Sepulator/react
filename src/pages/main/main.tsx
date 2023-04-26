13768ae0ff547fa7e0560eb4ad9dfd7dd4b12ce6
Yuri Skripal <yjura@protonmail.com>
2023-04-26 10:32:53 +0300

    feat: add suspense

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,13 +1,18 @@
-import { Footer } from '../../components/footer/footer';
-import { Header } from '../../components/header/header';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+
+import { Footer } from '@/components/footer/footer';
+import { Header } from '@/components/header/header';
+import { Spinner } from '@/components/icons';
 
 export const Main = () => {
   return (
     <>
       <Header />
       <main className="container">
-        <Outlet />
+        <Suspense fallback={<Spinner />}>
+          <Outlet />
+        </Suspense>
       </main>
       <Footer />
     </>
